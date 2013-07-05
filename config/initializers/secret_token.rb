12b8a87f5fa996c2fbc2923dad9101a1340413bb
Doug Phung <Doug@chillportal.com>
2013-07-05 00:31:59 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Floofydoug::Application.config.secret_token = '0a8a1364a7fba5de1cb64ede7680ca62368efbf4879636f02df77caf7cf3f4ca1223f4fdd78420b134f2f7500e4bdc2d82594a98fdb6b7088dc471048a2d7389'
